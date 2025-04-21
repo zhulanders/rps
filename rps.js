182e6c6a8c337b87e57998a7dc1720f640f15527
Joshua Zhu <jzhu2003@gmail.com>
2025-04-21 15:07:41 -0700

    Added functionial ui

--- rps.js
+++ rps.js
@@ -10,48 +10,54 @@ function getComputerChoice(){
             return "scissors";
     }
 }
-function getHumanChoice(){
-    return prompt("Rock Paper Scissors!");
-}
-
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        switch(playRound(humanSelection, computerSelection)){
-            case 1:
-                humanScore++;
-                break;
-            case -1:
-                computerScore++;
-                break;
-        }
-        
-    }
-    if (humanScore == computerScore){
-        console.log("It's a tie!")
-    }
-    else if(humanScore > computerScore){
-        console.log("You won!")
-    }
-    else{
-        console.log("You lose!")
-    }
-}
 function playRound(human, computer){
     human = human.toLowerCase();
     if (human === computer ){
-        console.log("It's a tie!");
+        result.textContent = "It's a tie!";
     }
     else if ((human == "rock" && computer == "scissors") || (human == "paper" && computer == "rock")  || (human == "scissors" && computer == "paper")){
-        console.log("You win, " + human + " beats " + computer + "!");
-        return 1;
+        result.textContent ="You win, " + human + " beats " + computer + "!";
+        humanScore++;
     }
     else {
-        console.log("You lose, " + computer + " beats " + human + "!");
-        return -1;
+        result.textContent = "You lose, " + computer + " beats " + human + "!";
+        compScore++;
     }
 }
-playGame();
+const choice = document.querySelector("#choice");
+const container = document.querySelector("#container");
+let humanScore = 0;
+let compScore = 0;
+const human = document.createElement("p");
+human.textContent = "Your Score: " + humanScore;
+const comp = document.createElement("p");
+comp.textContent = "Computer Score: " + compScore;
+const result = document.createElement("p");
+container.appendChild(human);
+container.appendChild(comp);
+container.appendChild(result);
+choice.addEventListener('click', (event) =>{
+    let target = event.target;
+    switch(target.id){
+        case 'Rock':
+            playRound('rock', getComputerChoice());
+            break;
+        case 'Paper':
+            playRound('paper', getComputerChoice());
+            break;
+        case 'Scissors':
+            playRound('scissors', getComputerChoice());
+            break;
+    }  
+    human.textContent = "Your Score: " + humanScore; 
+    comp.textContent = "Computer Score: " + compScore;
+    if (humanScore >= 5){
+        result.textContent = "You won the game! Congrats!";
+        choice.innerHTML = '';
+        
+    }
+    else if (compScore >= 5){
+        result.textContent = "You lost the game... Better luck next time.";
+        choice.innerHTML = '';
+    }
+});
